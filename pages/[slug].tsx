781e7df410b7d9926027a25363858ad058e447fc
Patrick Auld <patrick@patrickauld.com>
2021-07-23 21:04:35 +0000

    Adds README page and Markdown rendering

---
+++ pages/[slug].tsx
@@ -0,0 +1,85 @@
+import { useRouter } from 'next/router'
+import ErrorPage from 'next/error'
+import Container from '../components/container'
+import PostBody from '../components/post-body'
+import PostHeader from '../components/post-header'
+import Layout from '../components/layout'
+import { getPostBySlug, getAllPosts } from '../lib/api'
+import PostTitle from '../components/post-title'
+import PostType from '../types/post'
+import Head from 'next/head'
+import markdownToHtml from '../lib/markdownToHtml'
+
+type Props = {
+  post: PostType
+}
+
+const Post = ({ post }: Props) => {
+  const router = useRouter()
+  if (!router.isFallback && !post?.slug) {
+    return <ErrorPage statusCode={404} />
+  }
+  return (
+    <Layout>
+      <Container>
+        {router.isFallback ? (
+          <PostTitle>Loading…</PostTitle>
+        ) : (
+          <>
+            <article className="mb-32">
+              <Head>
+                <title>
+                  {post.title} | Patrick Auld
+                </title>
+              </Head>
+              <PostHeader title={post.title} />
+              <PostBody content={post.content} />
+            </article>
+          </>
+        )}
+      </Container>
+    </Layout>
+  )
+}
+
+
+export default Post
+
+type Params = {
+  params: {
+    slug: string
+  }
+}
+
+export async function getStaticProps({ params }: Params) {
+  const post = getPostBySlug(params.slug, [
+    'title',
+    'slug',
+    'content',
+  ])
+  const content = await markdownToHtml(post.content || '')
+
+  return {
+    props: {
+      post: {
+        ...post,
+        content,
+      },
+    },
+  }
+}
+
+export async function getStaticPaths() {
+  const posts = getAllPosts(['slug'])
+
+  return {
+    paths: posts.map((posts) => {
+      return {
+        params: {
+          slug: posts.slug,
+        },
+      }
+    }),
+    fallback: false,
+  }
+}
