de856a614c4d06c81a9d4a7c7d27f929f9b7a2e1
Patrick Auld <patrick@patrickauld.com>
2023-11-01 22:24:40 -0700

    Updates remark parser for next version

--- lib/markdownToHtml.tsx
+++ lib/markdownToHtml.tsx
@@ -1,7 +1,12 @@
-import remark from 'remark'
-import html from 'remark-html'
+import remarkHtml from 'remark-html'
+import remarkParse from 'remark-parse'
+import {read} from 'to-vfile'
+import {unified} from 'unified'
 
 export default async function markdownToHtml(markdown: string) {
-  const result = await remark().use(html).process(markdown)
-  return result.toString()
+  const result = await unified()
+    .use(remarkParse)
+    .use(remarkHtml)
+    .process(markdown)
+  return String(result)
 }
