5ede2819b31572b97c909b2fa5c9f40493a4b708
Patrick Auld <patrick@patrickauld.com>
2025-08-07 00:04:23 -0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   purge: ['./components/**/*.tsx', './pages/**/*.tsx'],
-  darkMode: 'media', // or 'media' or 'class'
+  darkMode: false, // or false or 'class'
   theme: {
     container: {
       center: true,
