3d7e2c8baf33ddaff3f57e5b3e199a5fa6692742
Patrick Auld <patrick@patrickauld.com>
2025-08-12 12:18:59 -0700

    Fixes darkmode for markdown pages

--- lib/markdownToHtml.tsx
+++ lib/markdownToHtml.tsx
@@ -1,12 +1,13 @@
-import remarkHtml from 'remark-html'
 import remarkParse from 'remark-parse'
-import {read} from 'to-vfile'
+import remarkRehype from 'remark-rehype'
+import rehypeStringify from 'rehype-stringify'
 import {unified} from 'unified'
 
 export default async function markdownToHtml(markdown: string) {
   const result = await unified()
     .use(remarkParse)
-    .use(remarkHtml)
+    .use(remarkRehype, { allowDangerousHtml: true })
+    .use(rehypeStringify, { allowDangerousHtml: true })
     .process(markdown)
   return String(result)
 }
